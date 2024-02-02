7ec7309e35f7565067d18b1b71d56211df2d1590
Neurs <31346263+Neurs12@users.noreply.github.com>
2024-02-02 19:53:50 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,6 @@
+$username = Read-Host "Your username"
+$uuid = [guid]::NewGuid().ToString() -creplace "-", ""
+
 echo "Downloading required resources..."
 (New-Object Net.WebClient).DownloadFile("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.msi", "$pwd\OpenJDK21.msi")
 (New-Object Net.WebClient).DownloadFile("https://skmedix.pl/bin/skl/3.2.5/x64/SKlauncher-3.2.exe", "$pwd\SKLauncher.exe")
@@ -6,17 +9,24 @@ echo "Downloading required resources..."
 echo "Installing JRE..."
 msiexec /i OpenJDK21.msi ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome,FeatureOracleJavaSoft INSTALLDIR="$pwd\jre" /quiet | Out-Null
 
-echo "Extracting + adding custom packages to \.minecraft..."
+echo "Extracting + adding custom config to \.minecraft..."
 
 Expand-Archive "$pwd\fast-pack.zip" -DestinationPath "$pwd\fast-pack"
+
+$accountContent = Get-Content -Path "$pwd\fast-pack\SKLauncher\accounts.json" -Raw
+
+$accountContent = $accountContent -creplace "#USERNAME", $username
+$accountContent = $accountContent -creplace "#UUID", $uuid
+$accountContent = $accountContent -creplace "#REFRESH", ([guid]::NewGuid().ToString() -creplace "-", "")
+
+Set-Content -Path "$pwd\fast-pack\SKLauncher\accounts.json" -Value $accountContent
+
 if (Test-Path -Path "$env:APPDATA\.minecraft") {
     Copy-Item -Path "$pwd\fast-pack\*" -Destination "$env:APPDATA\.minecraft" -Recurse -Force
 } else {
     New-Item -Path "$env:APPDATA\.minecraft" -ItemType Directory -Force
     Copy-Item -Path "$pwd\fast-pack\*" -Destination "$env:APPDATA\.minecraft" -Recurse -Force
 }
-
-$env:EXE4J_JAVA_HOME += ";$pwd\jre\bin"
 
 echo "Launching SKLauncher..."
 ."$pwd\SKLauncher"
