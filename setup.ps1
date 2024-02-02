5f227de46b981656558c7bce77d526836b58e928
Neurs <31346263+Neurs12@users.noreply.github.com>
2024-02-02 19:58:24 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,7 +4,7 @@ $uuid = [guid]::NewGuid().ToString() -creplace "-", ""
 echo "Downloading required resources..."
 (New-Object Net.WebClient).DownloadFile("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.msi", "$pwd\OpenJDK21.msi")
 (New-Object Net.WebClient).DownloadFile("https://skmedix.pl/bin/skl/3.2.5/x64/SKlauncher-3.2.exe", "$pwd\SKLauncher.exe")
-(New-Object Net.WebClient).DownloadFile("https://github.com/Neurs12/fast-pack/raw/main/fast-pack.zip", "$pwd\fast-pack.zip")
+(New-Object Net.WebClient).DownloadFile("https://fpack.pages.dev/fast-pack.zip", "$pwd\fast-pack.zip")
 
 echo "Installing JRE..."
 msiexec /i OpenJDK21.msi ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome,FeatureOracleJavaSoft INSTALLDIR="$pwd\jre" /quiet | Out-Null
