d7b3298f0f2244e83edbe1db7bf5766bbe2c32f2
Neurs <31346263+Neurs12@users.noreply.github.com>
2024-02-04 12:23:34 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -14,25 +14,43 @@ echo "[MAIN] Creating tasks to download required resources..."
 $runspacePool = [runspacefactory]::CreateRunspacePool(1, [Environment]::ProcessorCount)
 $runspacePool.Open()
 
+echo "[TASK] Downloading JRE 21..."
 $jre = [PowerShell]::Create().AddScript($downloadScript)
 $jre.AddArgument("[TASK] JRE 21 Downloaded.")
 $jre.AddArgument("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.msi")
 $jre.AddArgument("$pwd\OpenJDK21.msi")
 $jre.RunspacePool = $runspacePool
+$jre.Streams.Error.Clear()
+$jre.Streams.Warning.Clear()
+$jre.Streams.Information.Clear()
+$jre.Streams.Verbose.Clear()
+$jre.Streams.Debug.Clear()
 $jreRunner = $jre.BeginInvoke()
 
+echo "[TASK] Downloading SKLauncher..."
 $launcher = [PowerShell]::Create().AddScript($downloadScript)
 $launcher.AddArgument("[TASK] SKLauncher Downloaded.")
 $launcher.AddArgument("https://skmedix.pl/bin/skl/3.2.5/x64/SKlauncher-3.2.exe")
 $launcher.AddArgument("$desktopPath\SKLauncher.exe")
 $launcher.RunspacePool = $runspacePool
+$launcher.Streams.Error.Clear()
+$launcher.Streams.Warning.Clear()
+$launcher.Streams.Information.Clear()
+$launcher.Streams.Verbose.Clear()
+$launcher.Streams.Debug.Clear()
 $launcherRunner = $launcher.BeginInvoke()
 
+echo "[TASK] Downloading fast packages..."
 $fastPack = [PowerShell]::Create().AddScript($downloadScript)
 $fastPack.AddArgument("[TASK] Fast packages downloaded.")
 $fastPack.AddArgument("https://github.com/Neurs12/fast-pack/releases/latest/download/fast-pack.zip")
 $fastPack.AddArgument("$pwd\fast-pack.zip")
 $fastPack.RunspacePool = $runspacePool
+$fastPack.Streams.Error.Clear()
+$fastPack.Streams.Warning.Clear()
+$fastPack.Streams.Information.Clear()
+$fastPack.Streams.Verbose.Clear()
+$fastPack.Streams.Debug.Clear()
 $fastPackRunner = $fastPack.BeginInvoke()
 
 $jre.EndInvoke($jreRunner)
