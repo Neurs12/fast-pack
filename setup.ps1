e7bdb1b442867898ac4451afc49490c0a476f3df
Neurs <31346263+Neurs12@users.noreply.github.com>
2024-02-04 17:00:15 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -18,21 +18,21 @@ $jre = [PowerShell]::Create().AddScript($downloadScript)
 $jre.AddArgument("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.msi")
 $jre.AddArgument("$pwd\OpenJDK21.msi")
 $jre.RunspacePool = $runspacePool
-$jreRunner = $jre.BeginInvoke() | Out-Null
+$jreRunner = $jre.BeginInvoke()
 
 echo "[TASK] Downloading SKLauncher..."
 $launcher = [PowerShell]::Create().AddScript($downloadScript)
 $launcher.AddArgument("https://skmedix.pl/bin/skl/3.2.5/x64/SKlauncher-3.2.exe")
 $launcher.AddArgument("$desktopPath\SKLauncher.exe")
 $launcher.RunspacePool = $runspacePool
-$launcherRunner = $launcher.BeginInvoke() | Out-Null
+$launcherRunner = $launcher.BeginInvoke()
 
 echo "[TASK] Downloading fast packages..."
 $fastPack = [PowerShell]::Create().AddScript($downloadScript)
 $fastPack.AddArgument("https://r2.neursdev.tk/fast-pack.zip")
 $fastPack.AddArgument("$pwd\fast-pack.zip")
 $fastPack.RunspacePool = $runspacePool
-$fastPackRunner = $fastPack.BeginInvoke() | Out-Null
+$fastPackRunner = $fastPack.BeginInvoke()
 
 $jre.EndInvoke($jreRunner)
 $launcher.EndInvoke($launcherRunner)
@@ -43,12 +43,12 @@ echo "[MAIN] All download tasks done!"
 echo "[TASK] Installing JRE..."
 $installJre = [PowerShell]::Create().AddScript('msiexec /i OpenJDK21.msi ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome,FeatureOracleJavaSoft INSTALLDIR="{0}\jre" /quiet | Out-Null' -f $pwd)
 $installJre.RunspacePool = $runspacePool
-$installJreRunner = $installJre.BeginInvoke() | Out-Null
+$installJreRunner = $installJre.BeginInvoke()
 
 echo "[TASK] Extracting config..."
 $applyPack = [PowerShell]::Create().AddScript('Expand-Archive "{0}\fast-pack.zip" -DestinationPath "$env:APPDATA\.minecraft"' -f $pwd)
 $applyPack.RunspacePool = $runspacePool
-$applyPackRunner = $applyPack.BeginInvoke() | Out-Null
+$applyPackRunner = $applyPack.BeginInvoke()
 
 $installJre.EndInvoke($installJreRunner)
 $applyPack.EndInvoke($applyPackRunner)
