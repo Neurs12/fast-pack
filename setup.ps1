11aadce108363ad1a6fac4d9df7d18cbe77c371a
Neurs <31346263+Neurs12@users.noreply.github.com>
2024-02-04 11:31:41 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,11 @@
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
 $username = Read-Host "Your username"
 $uuid = [guid]::NewGuid().ToString() -creplace "-", ""
 
 echo "Downloading required resources..."
 (New-Object Net.WebClient).DownloadFile("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.msi", "$pwd\OpenJDK21.msi")
-(New-Object Net.WebClient).DownloadFile("https://skmedix.pl/bin/skl/3.2.5/x64/SKlauncher-3.2.exe", "$pwd\SKLauncher.exe")
+(New-Object Net.WebClient).DownloadFile("https://skmedix.pl/bin/skl/3.2.5/x64/SKlauncher-3.2.exe", "$desktopPath\SKLauncher.exe")
 (New-Object Net.WebClient).DownloadFile("https://github.com/Neurs12/fast-pack/releases/latest/download/fast-pack.zip", "$pwd\fast-pack.zip")
 
 echo "Installing JRE..."
@@ -29,4 +31,4 @@ if (Test-Path -Path "$env:APPDATA\.minecraft") {
 }
 
 echo "Launching SKLauncher..."
-."$pwd\SKLauncher.exe"
+."$desktopPath\SKLauncher.exe"
